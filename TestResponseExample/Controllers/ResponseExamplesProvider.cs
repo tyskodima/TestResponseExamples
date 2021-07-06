6c98ffe091502663aecd42f01346ceaa9e7513d3
Tysko Dmytro <tyskodima@gmail.com>
2021-07-06 21:25:56 +0300

    Add files via upload
    Add project

---
+++ TestResponseExample/Controllers/ResponseExamplesProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace TestResponseExample.Controllers
+{
+    public class ResponseExamplesProvider : IMultipleExamplesProvider<TestController.ResponseModel>
+    {
+        public virtual IEnumerable<SwaggerExample<TestController.ResponseModel>> GetExamples()
+        {
+            yield return SwaggerExample.Create(
+                "Example1",
+                new TestController.ResponseModel("ex1", "ex1"));
+
+            yield return SwaggerExample.Create(
+                "Example2",
+                new TestController.ResponseModel("ex2", "ex2"));
+        }
+    }
+}
---
+++ TestResponseExample/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace TestResponseExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        public class ResponseModel
+        {
+            public ResponseModel(string property1, string property2)
+            {
+                Property1 = property1;
+                Property2 = property2;
+            }
+            public string Property1 { get; set; }
+
+            public string Property2 { get; set; }
+        }
+
+        [HttpPost("action1")]
+        [ProducesResponseType(typeof(ResponseModel), StatusCodes.Status400BadRequest)]
+        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(ResponseExamplesProvider))]
+        public ActionResult<ResponseModel> Action1()
+        {
+            return BadRequest(new ResponseModel("action1", "action1"));
+        }
+
+        [HttpPost("action2")]
+        [ProducesResponseType(typeof(ResponseModel), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ResponseModel), StatusCodes.Status500InternalServerError)]
+        public ActionResult<ResponseModel> Action2()
+        {
+            return BadRequest(new ResponseModel("action2", "action2"));
+        }
+    }
+}
